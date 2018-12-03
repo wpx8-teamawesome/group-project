a6a3e6453d4346b7bb149479fc51c8bf5193df6d
EthanHess <ech1988@gmail.com>
2018-12-03 14:02:14 -0700

    adds routes and components

---
+++ src/components/LandingParent/LandingParent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './LandingParent.css'; 
+
+class LandingParent extends Component {
+    constructor() {
+        super() 
+        this.state = {
+            //User ect.
+        }
+    }
+
+    render() {
+        return (
+            <main className="Main_container">
+                <div className="Main_one">
+                
+                </div>
+                <div className="Main_two">
+                
+                </div>
+                <div className="Main_three">
+                
+                </div>
+                <div className="Main_four">
+                
+                </div>
+            </main>
+        )
+    }
+}
+
+export default LandingParent
--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Redux
+import { Provider } from 'react-redux';
+import store from './ducks/store';
+
+//Routing
+import { HashRouter } from 'react-router-dom'; 
+
+ReactDOM.render(<Provider store={store}>
+    <HashRouter>
+    <App />
+    </HashRouter>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
