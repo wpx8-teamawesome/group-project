da6a967ca3f33a626c3c02fc9cd903e91fec49d7
EthanHess <ech1988@gmail.com>
2018-12-03 16:07:29 -0700

    sets up new lobby branch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,42 @@ import Banner from './components/Banner/Banner.js';
 import routes from './routes'; 
 
 class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      scrolling : false, 
+      user: null,
+    }
+    this.handleScroll = this.handleScroll.bind(this)
+  }
+
+  //Listen for scrolling
+  addListener() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  removeListener() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll() {
+    if (window.scrollY === 0 && this.state.scrolling === true) {
+      this.setState({scrolling: false});
+    }
+    else if (window.scrollY !== 0 && this.state.scrolling !== true) {
+      this.setState({scrolling: true});
+    }
+  }
+
+  //Lifecycle
+  componentDidMount() {
+    this.addListener() 
+  }
+
+  componentWillUnmount() {
+    this.removeListener()
+  }
+
   render() {
     return (
       <div>
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
 import './Banner.css'; 
+import { connect } from 'react-redux';
+import { loginUser } from '..//../ducks/reducer'; 
+import { logoutUser } from '..//../ducks/reducer'; 
+//import { withRouter } from 'react-router-dom';
+
+import user from '..//..//images/user.png'; 
+import globe from '..//..//images/globe.png'; 
 
 class Banner extends Component {
     constructor() {
         super() 
-
+        this.state = {
+            user: null
+        }
     }
 
     render() {
         return (
             <header className="App-header">
-
+                <div className="left_nav">
+                    <p>Code Planet</p>
+                    <img src={globe}/>
+                </div>
+                <div className="right_nav">
+                    <img src={user}/>
+                </div>
             </header>
         )
     }
 }
 
-export default Banner
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, { loginUser, logoutUser })(Banner); 
