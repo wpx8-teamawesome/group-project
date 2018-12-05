38bd301f695a0cfd6ababdda26b834e60a335225
tstrat <tstrat@email.arizona.edu>
2018-12-05 12:51:34 -0700

    component creation for chat lobby and messages

--- src/components/ChatLobby/ChatLobby.js
+++ src/components/ChatLobby/ChatLobby.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import socketIOClient from "socket.io-client";
 import Message from './Message/Message';
 import './chatLobby.css';
-let socket = socketIOClient();
+let socket = socketIOClient('http://localhost:4000');  // update to match when on production
 
 
 export default class ChatLobby extends Component {
@@ -12,50 +12,76 @@ export default class ChatLobby extends Component {
         this.state = {
             message: "",
             messages: [],
-            joined: false
+            room: 'test-lobby', // set to `event-${this.props.event.id}` when ready to launch
+            joined: false,
+
         }
         socket.emit('join', {
-            id: 1,
-            socket_room: "event-1"
+            id: this.props.eventId,
+            socket_room: this.state.room
         });
 
         socket.on('message', message => {
+            /*
+                // for when we set up users so that 
+                // the current message that they just wrote
+                // gets displayed but doesn't stop people from
+                // scrolling through the chat every time someone posts
+                // something new.
+
+                if (message.user_id === my_user_id) {
+                    updateScroll()
+                }
+            */
             this.setState({
                 messages: [ ...this.state.messages, message]
-            }, this.updateScroll);
+            });
         })
 
         socket.on('chat-history', messageList => {
             this.setState({
-                messages: messageList
+                messages: messageList,
+                joined: true
             }, this.updateScroll)
         })
+
     }
 
 
     sendMessage = () => {
-        const { message } = this.state;
+        console.log('hit');
+        const { message, room } = this.state;
         socket.emit('new message', {
-            room: 'event-1',
+            room,
             userId: 1,
             name: 'travis',
             message,
             img: ''
         });
+
+        // Clear input
         this.setState({
             message: ''
         })
     }
 
+    /**
+     * updateScroll()
+     *      adjusts user view to start at the most recent message.  If
+     *      they send a message the update will also auto scroll to the bottom to see the new
+     *      message
+     */
     updateScroll = () => {
         var element = document.getElementById("messages");
         element.scrollTop = element.scrollHeight;
     }
 
     render() {
-        const { message, messages } = this.state;
+        const { message, messages, joined } = this.state;
+
         const messageDisplay = messages.map(m => 
             <Message 
+                key={m.id}
                 name={m.name} 
                 time={m.time}
                 message={m.message} 
@@ -64,17 +90,26 @@ export default class ChatLobby extends Component {
         );
         return (
             <div className="lobby-container">
-                <div className="chat-box">
-                    <div className="chat-title">TITLE</div>
-                    <div className="message-display" id='messages'>
-                        { messageDisplay }
+                { 
+                    !joined ?
+                    <div className="loading"></div> 
+                    : 
+                    <div className="chat-box">
+                        
+                        <div className="chat-title">TITLE</div>
+                        <div className="message-display" id='messages'>
+                            { messageDisplay }
+                        </div>
+                        <div className="input-box">
+                            <p style={{'color':'white'}}>~$</p>
+                            <input 
+                                value={message} 
+                                onChange={e => this.setState({ message: e.target.value })}
+                                onKeyUp={e => e.key==='Enter' ? this.sendMessage() : null } />
+                            <button onClick={this.sendMessage}>Send</button>
+                        </div>
                     </div>
-                    <div className="input-box">
-                        <input value={message} onChange={e => this.setState({ message: e.target.value })} />
-                        <button onClick={this.sendMessage}>Click Me</button>
-                    </div>
-
-                </div>
+                }
             </div>
         );
     }
