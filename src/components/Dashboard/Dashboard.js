9043bd61acd31e30c278afc7038d1ce541831d72
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-12-06 15:34:01 -0700

    Added EventCard Map Function

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import './dashboard.scss'
+import EventCard from '../EventCard/EventCard'
 
 class Dashboard extends Component {
   render() {
+      const eventsArray = [1,2,3,4,5,6,7,8,9,10,11,12]
     return (
       <div className="dashboard-container">
         <div className="dashboard-top-box">
             <button>Add/Edit <br/> Event</button>
             <h1>Nerd-Board</h1>
             <div className="dashboard-top-box-filler"></div>
+        
+        </div>
+
+        <div className="events">
+            <EventCard events={eventsArray}/>
         
         </div>
       </div>
--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function EventCard() {
-  return (
-    <div>
-      
-    </div>
-  )
+export default function EventCard(props) {
+    console.log(props)
+    const eachEvent = props.events.map((event, i) => {
+        return <h1 classname="white-text">{i}</h1>
+    })
+        return (
+            <div classname="event-card-container">
+              {eachEvent}
+            </div>
+          )
+    
+  
 }
