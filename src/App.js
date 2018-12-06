13e99bc8641393aafee07ec2de240751a5b73f10
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-12-06 16:03:33 -0700

    Temp Commit

--- src/App.js
+++ src/App.js
@@ -16,11 +16,3 @@ class App extends Component {
 }
 
 export default App;
-
-//COLOR SCHEME
-
-// Teal: #27E5E9
-// Blue/Gray: #27303F,
-// Dark Gray: #1A202B,
-// Regular blue: #1896DA,
-// Darkest color: #0E131B
