95f0d6839d36d0ecc93249e115a4b67f32cfcfdd
tstrat <tstrat@email.arizona.edu>
2018-12-07 11:10:54 -0700

    Updated controller to handle jsonb

--- server/controllers/peopleController.js
+++ server/controllers/peopleController.js
@@ -4,7 +4,6 @@ module.exports = {
 
         req.app.get('db').find_user_profile({id})
         .then( users => {
-            console.log("-- users", users);
             if (users.length) {
                 const user = users[0] // get result
                 /* We dont want to expose sensitive info here so there should not be a password in user object */
@@ -14,7 +13,7 @@ module.exports = {
                     email: user.email,
                     img: user.img,
                     name: user.name || user.username, // in case they did not set a name
-                    socialList: JSON.parse(user.social_list)
+                    socialList: user.social_list
                 }
                 res.status(200).json(payload);
             }
